e7bb884c36ebf611c44dd550517922574adee751
azrad <abdazrad.aa@gmail.com>
2015-09-17 23:00:40 -0700

    added cpu count/utilities import

--- FindDupes.py
+++ FindDupes.py
@@ -2,11 +2,12 @@ from __future__ import print_function  # print() support for python 2.7
 from nbt140 import NBTFile
 from glob import glob
 import os
-
+import utilities
 
 def main(player_folder):
     player_list = glob(os.path.join(player_folder, '*.dat'))
-
+    number_of_workers = utilities.get_worker_count()
+    print(number_of_workers)
 
 
 __author__ = 'azrad'
