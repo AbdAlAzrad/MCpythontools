8df74dbf233478134db0fddcb7b86a0c91652ce4
azrad <abdazrad.aa@gmail.com>
2015-09-19 07:58:32 -0700

    minor pep8 changes

--- utilities.py
+++ utilities.py
@@ -2,6 +2,7 @@ import os.path
 from multiprocessing import cpu_count
 from glob import glob
 
+
 def get_worker_count():
     """Set the number of processes to use, minimum of 1 process"""
     output_number_of_workers = cpu_count() - 1
